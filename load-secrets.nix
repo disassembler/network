fd7635e3d6b5dce62db655fa1cd35241fab75c80
Samuel Leathers <samuel.leathers@iohk.io>
2018-09-08 00:03:09 -0400

    fixing secrets

--- load-secrets.nix
+++ load-secrets.nix
@@ -1,5 +1,5 @@
 if builtins.pathExists ./secrets.nix then import ./secrets.nix else {
   prophet-openvpn-config = "";
-  prophet-gueest-openvpn-config = "";
+  prophet-guest-openvpn-config = "";
   centrallake-openvpn-config = "";
 }
