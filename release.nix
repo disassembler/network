74c188ae33105fe7adc38107cca07534245a4e1d
Samuel Leathers <samuel.leathers@iohk.io>
2018-09-08 01:31:34 -0400

    adding darwin tools and machines

--- release.nix
+++ release.nix
@@ -1,9 +1,13 @@
 let
   nixosFuncStable = (import <nixpkgs-stable/nixos>);
   nixosFuncUnstable = (import <nixpkgs-unstable/nixos>);
+  nixDarwinFuncUnstable = (import <nixdarwin>);
+  nix-darwin-tools = import ./.;
 
 in {
+  inherit nix-darwin-tools;
   optina = (nixosFuncStable { configuration = ./optina; }).system;
   portal = (nixosFuncStable { configuration = ./portal; }).system;
   sarov  = (nixosFuncUnstable { configuration = ./machines/sarov.nix; }).system;
+  ohrid  = (nixDarwinFuncUnstable { configuration = ./ohrid; }).system;
 }
